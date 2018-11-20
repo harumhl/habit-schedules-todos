863d2ef945b9912f60047e499cb71956d524de66
Haru Lee <haru.mhl@gmail.com>
2018-11-19 22:13:25 -0600

    adding spring boot backend

---
+++ spring-boot-backend/src/main/java/regression-on-stock-market/Greeter.java
@@ -0,0 +1,7 @@
+package RegressionOnStockMarket;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ spring-boot-backend/src/main/java/regression-on-stock-market/Main.java
@@ -0,0 +1,9 @@
+package RegressionOnStockMarket;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
+
