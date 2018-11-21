fb3496a70b2a9fa60ca384901167d8a37e9c08ad
Haru Lee <haru.mhl@gmail.com>
2018-11-20 20:34:34 -0600

    adding a new spring boot application, generated by initialyzer or whatever

---
+++ spring-boot-backend/src/main/java/org/haru/regressionOnStockMarket/RegressionOnStockMarketApplication.java
@@ -0,0 +1,12 @@
+package org.haru.regressionOnStockMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegressionOnStockMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegressionOnStockMarketApplication.class, args);
+	}
+}
