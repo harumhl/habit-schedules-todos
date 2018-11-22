77aab92604c2a7e3c8d11eb575d6df6154bc867b
Haru Lee <haru.mhl@gmail.com>
2018-11-21 18:36:26 -0600

    disabling basic spring security

---
+++ spring-boot-backend/src/main/java/org/haru/regressionOnStockMarket/RegressionOnStockMarketSecurityConfig.java
@@ -0,0 +1,18 @@
+package org.haru.regressionOnStockMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@EnableWebSecurity
+public class RegressionOnStockMarketSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+            .antMatchers("/**").permitAll();
+    }
+}
